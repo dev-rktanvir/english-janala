e5e58538aeece0d11841a6077b8d599eae1e0340
rk-tanvir790 <ktanvir790@gmail.com>
2026-02-15 01:26:28 +0600

    add banner for empty lesson

--- src/script/english-janala.js
+++ src/script/english-janala.js
@@ -1,51 +1,61 @@
 // Load Lesson Button function
 const loadLessonBtn = () => {
-    const url = 'https://openapi.programming-hero.com/api/levels/all';
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayLessonBtn(data.data))
+  const url = 'https://openapi.programming-hero.com/api/levels/all';
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayLessonBtn(data.data))
 }
 
 
 // Display Lesson Button Function
 const displayLessonBtn = (lessons) => {
-    // target div
-    const lessonBtn = document.getElementById("lessonBtn");
-    lessonBtn.innerHTML = "";
+  // target div
+  const lessonBtn = document.getElementById("lessonBtn");
+  lessonBtn.innerHTML = "";
 
-    // Loop through
-    lessons.forEach(lesson => {
-        // Create Div
-        const btnDiv = document.createElement("div");
-        btnDiv.innerHTML = `
+  // Loop through
+  lessons.forEach(lesson => {
+    // Create Div
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
         <button onclick="loadWordByLevel(${lesson.level_no})" class="btn btn-outline btn-primary">
           <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
         </button>
         `
-        // Append in LessonBtn
-        lessonBtn.append(btnDiv);
-    })
+    // Append in LessonBtn
+    lessonBtn.append(btnDiv);
+  })
 }
 
 // Load Word By Level
 const loadWordByLevel = (id) => {
-    const url = `https://openapi.programming-hero.com/api/level/${id}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayWordByLevel(data.data))
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayWordByLevel(data.data))
 }
 
 // Display Word By Level
 const displayWordByLevel = (words) => {
-    // target div
-    const lessonCard = document.getElementById("lessonCard");
-    lessonCard.innerHTML = "";
+  // target div
+  const lessonCard = document.getElementById("lessonCard");
+  lessonCard.innerHTML = "";
 
-    // Loop Through
-    words.forEach(word => {
-        // Create Element
-        const cardContainer = document.createElement("div");
-        cardContainer.innerHTML = `
+  if (words.length == 0) {
+    lessonCard.innerHTML = `
+        <div class="bg-base-200 py-16 rounded-3xl col-span-full">
+          <img class="mx-auto" src="/src/assets/alert-error.png" alt="alert pic">
+          <h2 class="bangla-font text-sm text-[#79716B] py-4">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি। ।</h2>
+          <p class="bangla-font font-medium text-4xl text-secondary">নেক্সট Lesson এ যান</p>
+        </div>
+        `
+  }
+
+  // Loop Through
+  words.forEach(word => {
+    // Create Element
+    const cardContainer = document.createElement("div");
+    cardContainer.innerHTML = `
           <div class="bg-white rounded-xl p-8 h-full flex flex-col justify-between">
             <div>
               <h3 class="font-bold text-3xl text-secondary">${word.word}</h3>
@@ -65,9 +75,9 @@ const displayWordByLevel = (words) => {
           </div>
         `
 
-        // Append Element
-        lessonCard.append(cardContainer);
-    })
+    // Append Element
+    lessonCard.append(cardContainer);
+  })
 }
 
 
